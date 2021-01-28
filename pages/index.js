c2b592b3c377f047c3bc585266f3ddb563c696ff
Joe Blau <im@joeblau.com>
2021-01-27 18:34:42 -0800

    Update graphql models

--- pages/index.js
+++ pages/index.js
@@ -7,8 +7,24 @@ export default function Home({data}) {
   return (
     <div className={styles.container}>
       <Head>
-        <title>The Composable Architecture Kit</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta charSet="utf-8" />
+
+        <meta name="description" content={data.organization.description} />
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" key="twcard" />
+        <meta name="twitter:creator" content={data.organization.twitterUsername} key="twhandle" />
+
+        {/* Open Graph */}
+        <meta property="og:url" content={data.organization.websiteUrl} key="ogurl" />
+        <meta property="og:image" content={data.organization.avatarUrl} key="ogimage" />
+        <meta property="og:site_name" content={data.organization.name} key="ogsitename" />
+        <meta property="og:title" content={data.organization.name} key="ogtitle" />
+        <meta property="og:description" content={data.organization.description} key="ogdesc" />
+
+        <title>{data.organization.name}</title>
       </Head>
 
       <main className={styles.main}>
@@ -22,8 +38,8 @@ export default function Home({data}) {
 
         <div className={styles.grid}>
           {data.organization.repositories.nodes.map((repo, index) => {
-            return <Fragment>
-                <a href={repo.url} className={styles.card}>
+            return <Fragment key={index}>
+              <a href={repo.url} className={styles.card}>
                 <img src={repo.openGraphImageUrl}/>
                 <h3>{repo.name}</h3>
                 <p>{repo.description}</p>
@@ -51,7 +67,7 @@ export async function getStaticProps(context) {
         avatarUrl
         name
         twitterUsername
-        repositories(first: 20) {
+        repositories(first: 30, orderBy: {field: NAME, direction: ASC}) {
           nodes {
             url
             description
@@ -61,6 +77,7 @@ export async function getStaticProps(context) {
             name
           }
         }
+        websiteUrl
       }
     }
     `,
